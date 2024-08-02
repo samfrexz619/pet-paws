5e32a662e2ab7c639105a4ac8e115ccf7299103e
samfrexz619 <samfrexz69@gmail.com>
2024-08-02 20:26:53 +0100

    add empty state if filtered item is not found

--- src/pages/breeds/Breeds.tsx
+++ src/pages/breeds/Breeds.tsx
@@ -12,6 +12,7 @@ import BreedImage from "@/components/breeds/BreedImage";
 import { Skeleton } from "@/components/ui/skeleton";
 import { useCatApiContext } from "@/context/CatApiContext";
 import EmojiIcons from "@/components/ui/EmojiIcon";
+import EmptyState from "@/components/EmptyState";
 
 
 
@@ -93,21 +94,20 @@ const Breeds = () => {
       </div>
 
       <section className="w-full max-h-[75vh] overflow-y-scroll reset">
-        {loading ?
+        {loading &&
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3">
             <Skeleton className="rounded-10 w-full h-[250px] bg-grey" />
-
-          </div>
-          : (<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3">
-            {filteredBreeds?.slice(0, parseInt(selectVal)).map((breed, index) => (
-              <BreedImage
-                key={breed.id}
-                breedImg={breed}
-                index={index}
-              />
-            ))}
-          </div>)
-        }
+          </div>}
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3">
+          {filteredBreeds?.slice(0, parseInt(selectVal)).map((breed, index) => (
+            <BreedImage
+              key={breed.id}
+              breedImg={breed}
+              index={index}
+            />
+          ))}
+        </div>
+        {filteredBreeds.length === 0 && <EmptyState />}
       </section>
     </section>
   );
